f47983e4ee139928bbef0ce53cf6bd9f14be10b4
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-09-21 15:00:41 -0300

    Lint

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 class App extends React.Component {
   render() {
     return (
-      <Header/>
+      <Header />
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 class Header extends React.Component {
-  render () {
+  render() {
     return (
       <h1>"Sistema Solar"</h1>
     );
