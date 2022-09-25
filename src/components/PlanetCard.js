55956e0b6c4555c4df1b2a858360a092ecbe889e
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-09-25 12:39:48 -0300

    Desafio 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Proptype from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: Proptype.arrayOf.isRequired,
+  planetImage: Proptype.arrayOf.isRequired,
+};
+
+export default PlanetCard;
