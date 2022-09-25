8af0d795ecb0d73b47141f1a79fba0af84d564ec
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-09-25 12:07:25 -0300

    Desafio 3

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Proptype from 'prop-types';
 
 class Title extends React.Component {
   render() {
-    return (
-      <title>
-        <h2>{ headline }</h2>
-      </title>
-    );
+    const { headline } = this.props;
+    return <h2>{ headline }</h2>;
   }
 }
+
+Title.propType = {
+  headline: Proptype.string.isRequired,
+};
 
 export default Title;
