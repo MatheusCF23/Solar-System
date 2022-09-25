ab278c3075ea8f0466ad3aabe06c65b118d1ac86
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-09-25 11:39:45 -0300

    En

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    return (
+      <title>
+        <h2>{ headline }</h2>
+      </title>
+    );
+  }
+}
+
+export default Title;
